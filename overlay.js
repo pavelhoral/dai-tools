d19f0bb343a3f86a6d4cec5a9d659c5e2205ee07
Pavel Horal <pavel.horal@orchitech.cz>
2018-04-14 18:20:20 +0200

    Info level messages.

--- overlay.js
+++ overlay.js
@@ -5,7 +5,8 @@ program.
     usage('[options] <source...>').
     description('Apply CSV translations.').
     option('-t, --target <directory>', 'target directory').
-    option('-l --lang <lang>', 'target language', 'en').
+    option('-l, --lang <lang>', 'target language', 'en').
+    option('-e, --empty', 'allow empty strings').
     parse(process.argv);
 
 if (!program.target || program.args.length < 1) {
@@ -31,17 +32,25 @@ program.args.forEach(file => {
 
 texttable.search(program.target, program.lang).forEach(table => {
     let changed = 0;
+    let missed = 0;
     let entries = [];
     texttable.parse(table.file).forEach((entry, line) => {
         let string = strings[entry[0] + ':' + entry[1]];
-        if (string) {
+        if (string && (program.empty || string['cs'])) {
             entry[1] = string['cs'];
             changed += 1;
+        } else if (entry[1] &&
+                !/^(\{string\}[0-9]+\{\/string\} ?)+$/.test(entry[1])  &&
+                !/^\{[^\}]+\}$/.test(entry[1])) {
+            missed += 1;
         }
         entries.push(entry);
     });
     if (changed) {
-        console.log(`DEBUG Updating ${changed} entries in ${table.file}.`);
+        console.log(`INFO Updating ${changed} entries in ${table.file}.`);
         texttable.write(table.file, entries);
+    }
+    if (missed) {
+        console.log(`INFO Missed ${missed} entries in ${table.file}.`);
     }
 });
